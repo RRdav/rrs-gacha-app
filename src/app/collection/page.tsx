3f30810c7ac75896811a102f7a3f5c0bb9f3217c
Raphael David <raphaelruizd@gmail.com>
2025-06-19 21:23:25 +1000

    Collections page user persistence

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,13 +1,40 @@
-'use client';
+
 
 import { CollectionContext } from "@/contexts/CollectionContext"
 import Image from "next/image";
 import Link from "next/link";
-import { useContext } from "react"
+import { createClient } from "../utils/supabase/server";
 
-export default function CollectionPage() {
+export default async function CollectionPage() {
 
-    const { acquiredCharacters } = useContext(CollectionContext);
+    // fetch user specific collection data
+    const supabase = await createClient();
+    const { data: userCollectionData, error } = await supabase.from("usercollection").select()
+
+
+    // fetch character data based on user collection id
+    const characterPromises = userCollectionData!.map(async (character, index) => {
+        const { data, error } = await supabase
+            .from("gachacharacters")
+            .select()
+            .eq('id', character.character_id)
+            .single();
+
+        if (error) {
+            console.error(`Error fetching character ${character.id}:`, error);
+            return null;
+        }
+        return data;
+    });
+
+    // Ensure that this variable is waiting for all promises to resolve
+    const resolvedCharacters = await Promise.all(characterPromises);
+
+    // Filter out any null values in case some characters were not found
+    const userAcquiredCharacters = resolvedCharacters.filter(character => character !== null);
+
+    console.log("Resolved Characters:", userAcquiredCharacters);
+
 
     return (
         <main className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -16,7 +43,7 @@ export default function CollectionPage() {
             </Link>
             <h1>Hello, this is where your collection will show up!</h1>
             <ul className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                {acquiredCharacters.map((character, index) => (
+                {userAcquiredCharacters.map((character, index) => (
                     <li key={index}>
                         <h2>{character.name}</h2>
                         <p>Rarity: {character.rarity}</p>
